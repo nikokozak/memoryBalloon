30190e27310959faebbb197491c2631e342995aa
nasif-co <nasifrincon96@gmail.com>
2024-12-09 20:14:30 -0500

    Init p5 sketch

---
+++ p5-mouse-complexity/sketch.js
@@ -0,0 +1,15 @@
+/*
+ * Exploring ideas on internal complexity by
+ * categorizing mouse movement based on 
+ * gentleness and roughness.
+ *
+ * 09/12/24 @ 8:12pm, ITP floor
+ */
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0,0,0);
+}
